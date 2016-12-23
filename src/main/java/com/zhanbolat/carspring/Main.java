2f305cde7c19e61d60d9713b27a546b108f4ec34
Zhanbolat <zhanbolat.mt@gmail.com>
2016-12-23 18:43:19 +0600

    add spring

--- src/main/java/com/zhanbolat/carspring/Main.java
+++ src/main/java/com/zhanbolat/carspring/Main.java
@@ -1,10 +1,13 @@
 package com.zhanbolat.carspring;
 
+import com.zhanbolat.carspring.cars.ICar;
+import com.zhanbolat.carspring.cars.impl.Golf3;
+
 /**
  * Created by Computer308 on 23.12.2016.
  */
 public class Main {
     public static void main(String ... args) {
-        
+
     }
 }
--- src/main/java/com/zhanbolat/carspring/cars/impl/Golf3.java
+++ src/main/java/com/zhanbolat/carspring/cars/impl/Golf3.java
@@ -3,9 +3,7 @@ package com.zhanbolat.carspring.cars.impl;
 import com.zhanbolat.carspring.cars.Car;
 import com.zhanbolat.carspring.cars.DriveType;
 import com.zhanbolat.carspring.engines.IEngine;
-import com.zhanbolat.carspring.engines.impl.AEE;
 import com.zhanbolat.carspring.transmissions.ITransmission;
-import com.zhanbolat.carspring.transmissions.impl.M_020;
 
 /**
  * Created by zhanbolat on 18.12.2016.
