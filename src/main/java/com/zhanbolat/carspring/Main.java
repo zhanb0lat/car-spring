fd6aa1045af2ba7530c780c4bee8a822fa2f6c91
Zhanbolat <zhanbolat.mt@gmail.com>
2016-12-27 12:26:28 +0600

    autowiring (xml)

--- src/main/java/com/zhanbolat/carspring/Main.java
+++ src/main/java/com/zhanbolat/carspring/Main.java
@@ -1,5 +1,6 @@
 package com.zhanbolat.carspring;
 
+import com.zhanbolat.carspring.cars.ICar;
 import com.zhanbolat.carspring.pool.IPool;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -12,14 +13,6 @@ public class Main {
 
     public static void main(String ... args) {
         context = new ClassPathXmlApplicationContext("config.xml");
-        IPool enginePool = (IPool) context.getBean("enginePool");
-
-        for (Object engine : enginePool.getItems())
-            System.out.println(engine);
-
-        IPool transmissionPool = (IPool) context.getBean("transmissionPool");
-
-        for (Object transmissoin : transmissionPool.getItems())
-            System.out.println(transmissoin);
+        ICar car = (ICar) context.getBean("Golf3");
     }
 }
